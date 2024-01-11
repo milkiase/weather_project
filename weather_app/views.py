ec68b4475ff5af89e7a6702c197b2f16a26a29ad
milkiase <milktes27@gmail.com>
2024-01-11 19:40:13 +0300

    prevented adding duplicate city names

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,6 +1,7 @@
 # weather_app/views.py
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import City
 from .utils import get_weather_data
 
@@ -11,9 +12,13 @@ def index(request):
 
 def add_city(request):
     if request.method == 'POST':
-        city_name = request.POST['city_name']
-        get_weather_data(city_name)
-    return HttpResponseRedirect('/')
+        city_name = request.POST['city_name'].strip()
+        
+        # Check if the city already exists in the database
+        if not City.objects.filter(name__iexact=city_name).exists():
+            get_weather_data(city_name)
+        
+    return HttpResponseRedirect(reverse('index'))
 
 def city_detail(request, city_id):
     city = get_object_or_404(City, pk=city_id)
