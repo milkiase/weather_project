1f79eba59c55bd163026cfa2020365fc65634ee2
milkiase <milktes27@gmail.com>
2024-01-11 19:54:51 +0300

    added autocomplete suggestions based on OpenWeatherMap's valid city names

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,9 +1,10 @@
 # weather_app/urls.py
 from django.urls import path
-from .views import index, add_city, city_detail
+from .views import index, add_city, city_detail, city_autocomplete
 
 urlpatterns = [
     path('', index, name='index'),
     path('add_city/', add_city, name='add_city'),
     path('city_detail/<int:city_id>/', city_detail, name='city_detail'),
+    path('city_autocomplete/', city_autocomplete, name='city_autocomplete'),
 ]
--- weather_app/views.py
+++ weather_app/views.py
@@ -2,6 +2,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+import requests
+from django.http import JsonResponse
+from django.db.models import Q
 from .models import City
 from .utils import get_weather_data
 
@@ -14,7 +17,7 @@ def add_city(request):
     if request.method == 'POST':
         city_name = request.POST['city_name'].strip()
         
-        # Check if the city already exists in the database
+        # Check if the city already exists in the database (case-insensitive)
         if not City.objects.filter(name__iexact=city_name).exists():
             get_weather_data(city_name)
         
@@ -25,3 +28,19 @@ def city_detail(request, city_id):
     weather_details = get_weather_data(city.name)
     context = {'city': city, 'weather_details': weather_details}
     return render(request, 'weather_app/city_detail.html', context)
+
+def city_autocomplete(request):
+    term = request.GET.get('term', None)
+    if term:
+        api_key = '11791edf82232148a9ce38dd62cd0dea'
+        url = f'http://api.openweathermap.org/geo/1.0/direct?q={term}&limit=5&appid={api_key}'
+        
+        try:
+            response = requests.get(url)
+            data = response.json()
+            suggestions = [city['name'] for city in data]
+            return JsonResponse(suggestions, safe=False)
+        except Exception as e:
+            print(f"Error fetching suggestions: {e}")
+
+    return JsonResponse([], safe=False)
